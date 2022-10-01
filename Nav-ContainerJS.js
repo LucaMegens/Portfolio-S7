25151e31096d884560ebccb59a11f11f3df98b7c
Luca Megens <mail@lucamegens.nl>
2022-10-01 17:54:58 +0200

    dng

---
+++ Nav-ContainerJS.js
@@ -0,0 +1,9 @@
+function OpenNav(){
+    document.getElementById("Main_Page").style.filter="blur(25px)"
+    document.getElementById("Nav-Container").style.display="flex"
+    
+}
+
+function CloseNav(){
+    document.getElementById("Nav-Container").style.display="none"
+}
