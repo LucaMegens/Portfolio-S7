6cb3b0a4b387039de1fb9582ae23f7b19bbb3770
Luca Megens <mail@lucamegens.nl>
2022-10-20 17:05:59 +0200

    nav stuff

--- Nav-ContainerJS.js
+++ Nav-ContainerJS.js
@@ -1,9 +1,12 @@
 function OpenNav(){
     document.getElementById("Main_Page").style.filter="blur(25px)"
     document.getElementById("Nav-Container").style.display="flex"
+    document.getElementById("Body").style.overflow="hidden"
     
 }
 
 function CloseNav(){
     document.getElementById("Nav-Container").style.display="none"
+    document.getElementById("Main_Page").style.filter="blur(0px)"
+    document.getElementById("Body").style.overflow="auto"
 }
