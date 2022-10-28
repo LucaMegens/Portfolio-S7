7974d02eae2239ac86cbcaefc7d8bb72874b413d
Luca Megens <mail@lucamegens.nl>
2022-10-28 16:03:26 +0200

    Fixed Navigation, Added style for all pages

--- Nav-ContainerJS.js
+++ Nav-ContainerJS.js
@@ -2,11 +2,12 @@ function OpenNav(){
     document.getElementById("Main_Page").style.filter="blur(25px)"
     document.getElementById("Nav-Container").style.display="flex"
     document.getElementById("Body").style.overflow="hidden"
-    
+    document.getElementById("Nav-Container-Hide").style.display="none"
 }
 
 function CloseNav(){
-    document.getElementById("Nav-Container").style.display="none"
     document.getElementById("Main_Page").style.filter="blur(0px)"
+    document.getElementById("Nav-Container").style.display="none" 
     document.getElementById("Body").style.overflow="auto"
+    document.getElementById("Nav-Container-Hide").style.display="flex"
 }
